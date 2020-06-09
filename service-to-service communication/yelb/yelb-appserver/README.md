e505fb593b0452c27385759b3df4fd275da3a6fa
gmridula <gmridula@amazon.com>
2020-06-09 10:33:46 -0400

    cleanup of files

--- service-to-service communication/yelb/yelb-appserver/README.md
+++
@@ -1,1 +0,0 @@
-This is the application server. It's a Ruby/Sinatra application that exposes a number of APIs that are consumed by the UI (or via curl if you fancy that). 
--- service-to-service communication/yelb/yelb-appserver/modules/getrecipe_dontchange.rb
+++
@@ -1,6 +0,0 @@
-
-def getrecipe()
-        #recipeurl ="https://www.allrecipes.com/recipe/60500/the-most-incredible-pancake-bites-ever/"
-        recipeurl = '{"recipelink": "'+ $recipeendpoint + '"' + "}"
-        return recipeurl
-end
